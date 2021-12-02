ade55f92b5270a1258ad5340a2c352de1cf55566
FaithMandela <faithmandela@gmail.com>
2021-12-03 01:05:32 +0300

    Retrieved data from the radio buttons

--- akanGenerator.js
+++ akanGenerator.js
@@ -1,4 +1,12 @@
 function generateName() {
     var date = document.getElementById("birthday").value;
-    console.log("Datetetet"+date);
+    var list_gender = document.getElementsByName("gender");
+    // Get the value of the selected button
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    // for(i=0;i<=list_gender.length;i++){
+    //   if(list_gender[i].ariaChecked){
+    //     var gender = list_gender[i].value;
+    //   }
+    // }
+    console.log("Datetetet"+gender);
   }
