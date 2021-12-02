ba5e3a229cc27c3337523d192c4191866ed36763
FaithMandela <faithmandela@gmail.com>
2021-12-03 02:08:43 +0300

    Updated  the formulae

--- akanGenerator.js
+++ akanGenerator.js
@@ -8,11 +8,21 @@ function generateName() {
 
     var MM = bdate.getMonth()+1;
     var DD = bdate.getDate();
-    var YY = bdate.getFullYear();
+    var YY1 = bdate.getFullYear();
+    var YY = YY1.toString().substr(-2);
+    //Get century digits
+    var CC = Math.floor(YY1/100);
+
+    //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+    //console.log(new Date().getFullYear().toString().substr(-2));
+    var dayBorn = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7
 
     console.log("Gender is "+gender);
     console.log("Date is "+ bdate);
     console.log("Month is "+MM);
     console.log("Day is "+DD);
     console.log("Year is "+YY);
+    console.log("YY1 is "+YY1)
+    console.log("Century is "+CC);
+    console.log("Day born is "+dayBorn);
   }
